ab06d87cf24c7c30030720ca921b4deb00430019
dark-neonus <nazarpas2007@gmail.com>
2024-07-20 00:09:08 +0300

    feat(auth): add all entry forms to register

--- connectior/auth.py
+++ connectior/auth.py
@@ -3,6 +3,7 @@ import functools
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from connectior.db import get_db
@@ -13,25 +14,34 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        nickname = request.form['nickname']
+        
         db = get_db()
         error = None
 
-        if not username:
-            error = 'Username is required.'
+        if not email:
+            error = 'Email is required.'
         elif not password:
             error = 'Password is required.'
+        elif not first_name:
+            error = 'First name is required.'
+        elif not nickname:
+            error = 'Nickname is required.'
+        
 
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
+                    "INSERT INTO user (email, password, first_name, last_name, nickname) VALUES (?, ?, ?, ?, ?)",
+                    (email, generate_password_hash(password), first_name, last_name, nickname),
                 )
                 db.commit()
             except db.IntegrityError:
-                error = f"User {username} is already registered."
+                error = f"User {email} is already registered."
             else:
                 return redirect(url_for("auth.login"))
 
