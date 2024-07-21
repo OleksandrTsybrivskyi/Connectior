8d5910dae54a1eb9948e0cc145d95344b8656255
Oleksandr <tsybrivsky@gmail.com>
2024-07-21 17:19:25 +0300

    feat(auth): add email verification

--- connectior/schema.sql
+++ connectior/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS users (
     first_name VARCHAR(16) NOT NULL,
     last_name VARCHAR(16),
     nickname VARCHAR(32) NOT NULL
-)
+);
 
 CREATE TABLE IF NOT EXISTS chats (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS chats (
     user_2 INTEGER NOT NULL,
     FOREIGN KEY (user_1) REFERENCES users (id),
     FOREIGN KEY (user_2) REFERENCES users (id)
-)
+);
 
 CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -24,4 +24,15 @@ CREATE TABLE IF NOT EXISTS messages (
     send_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (sender) REFERENCES users (id),
     FOREIGN KEY (chat_id) REFERENCES chats (id)
-)
+);
+
+CREATE TABLE IF NOT EXISTS unactivated_users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email VARCHAR(128) NOT NULL,
+    password VARCHAR(256) NOT NULL,
+    first_name VARCHAR(16) NOT NULL,
+    last_name VARCHAR(16),
+    nickname VARCHAR(32) NOT NULL,
+    activation_code TEXT NOT NULL,
+    sent_time INTEGER NOT NULL
+);
