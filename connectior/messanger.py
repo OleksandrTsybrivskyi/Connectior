53927c055b96ab21c33917a5052b284f38351a8c
dark-neonus <nazarpas2007@gmail.com>
2024-07-22 22:46:40 +0300

    chores(messanger.py): buggy commit to stay up to date

--- connectior/messanger.py
+++ connectior/messanger.py
@@ -1,8 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, url_for
+from connectior.db import get_db
+
+
 
 bp = Blueprint('messanger', __name__)
 
 
-@bp.route('/messanger')
+@bp.route('/messanger') 
 def messanger():
-    return render_template("messanger.html")
+    db = get_db()
+
+    # chats = db.execute(
+    #     """
+    #     SELECT chat.* messages.send_time
+    #     FROM chats
+    #     WHERE user_1 = ? OR user_2 = ?
+    #     ORDER BY (SELECT * FROM messages WHERE last_message_id)
+    #     """, (session['user_id'], session['user_id'])
+    # ).fetchall()
+
+    chats = db.execute(
+        """
+        SELECT chats.* messages.send_time
+        FROM chats
+        WHERE user_1 = ? OR user_2 = ?
+        ORDER BY (SELECT * FROM messages WHERE last_message_id) DESC
+        """, (session['user_id'], session['user_id'])
+    ).fetchall()
+
+    print(chats)
+
+    return render_template("messanger.html", chats=chats, logout_url=url_for('auth.logout'))
--- connectior/static/lib/verification.py
+++
@@ -1,8 +0,0 @@
-import regex as re
-
-# Email verification
-# Password verification
-# First name verification
-# Last name verification
-# Nickname verification
-
