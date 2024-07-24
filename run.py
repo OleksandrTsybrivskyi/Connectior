6170ebe2b9eddcefc9c94d1789a1e17764bc7d90
Oleksandr <tsybrivsky@gmail.com>
2024-07-24 14:53:04 +0300

    feat(run.py): add support for local network

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 from app import *
 
+import sys
+import config
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    PORT: int = 0
+
+    try:
+        PORT = int(sys.argv[1])
+    except IndexError:
+        PORT = config.DEFAULT_PORT
+        print("Setting to default port - {}")
+    except ValueError:
+        print("PORT must be an integer! setting to default port - {}")
+
+
+    socketio.run(app, host="0.0.0.0", port=PORT, debug=True)
